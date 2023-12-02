f0227b90aeca978d480e0caf3b51cb2bb6a06ea3
David Cobas <cobasesp.developer@gmail.com>
2023-12-02 12:26:13 +0100

    Clean catalogue component

--- src/app/components/catalogue/catalogue.component.ts
+++ src/app/components/catalogue/catalogue.component.ts
@@ -1,9 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
-
-// Interfaces
-import { Product } from 'src/app/interfaces/product';
 
 @Component({
   selector: 'app-catalogue',
@@ -14,11 +10,7 @@ export class CatalogueComponent implements OnInit {
 
   constructor(private api:ApiService) { }
 
-  public products: Product[] = [];
-
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   get Products() {
     return this.api.getAllProducts();
